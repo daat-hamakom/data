097d41285a56c0633b6a0dc743ad8801adf7bcc2
Yuval Adam <yuval@y3xz.com>
2015-11-11 16:29:31 +0200

    Add more event fields

--- daat/core/models.py
+++ daat/core/models.py
@@ -9,11 +9,45 @@ class DaatUser(AbstractUser):
     pass
 
 
+class Project(models.Model):
+    pass
+
+
+class Place(models.Model):
+    pass
+
+
+class Person(models.Model):
+    pass
+
+
+class Organization(models.Model):
+    pass
+
+
+class Media(models.Model):
+    pass
+
+
 class Event(models.Model):
+    MAP_CONTEXTS = (
+        ('neighbourhood', 'Neighbourhood'),
+        ('city', 'City'),
+        ('province', 'Province'),
+        ('country', 'Country'),
+        ('continent', 'Continent'),
+        ('world', 'World'),
+    )
+
     title = models.CharField(max_length=160)
     description = models.TextField(blank=True)
     start_date = models.CharField(max_length=10, validators=[partial_date_validator])
     end_date = models.CharField(max_length=10, blank=True, validators=[partial_date_validator])
+    place = models.ForeignKey(Place, related_name='events')
+    map_context = models.CharField(max_length=20, choices=MAP_CONTEXTS, blank=True)
+    people = models.ManyToManyField(Person, related_name='events')
+    organizations = models.ManyToManyField(Organization, related_name='events')
+    media = models.ManyToManyField(Media, related_name='events')
 
     def __str__(self):
         return self.title
