6d1f5ab1651b8ac894d21220786394e9ded7e54e
Yuval Adam <yuval@y3xz.com>
2015-11-11 15:26:48 +0200

    Add date validation tests

--- daat/core/models.py
+++ daat/core/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db import models
 
 from .utils import partial_date_validator
 
@@ -13,3 +14,8 @@ class Event(models.Model):
     description = models.TextField(blank=True)
     start_date = models.CharField(max_length=10, validators=[partial_date_validator])
     end_date = models.CharField(max_length=10, blank=True, validators=[partial_date_validator])
+
+    def clean(self):
+        if self.end_date:
+            if self.end_date < self.start_date:
+                raise ValidationError({'end_date': 'End date must occur after start date'})
--- daat/core/tests.py
+++ daat/core/tests.py
@@ -1,2 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from .models import *
+
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_event_dates(self):
+        Event.objects.create(title='Test Event', start_date='2015-11-11', end_date='2015-12-12').full_clean()
+        Event.objects.create(title='Test Event', start_date='2015-00-00', end_date='2016-00-00').full_clean()
+        with self.assertRaises(ValidationError):
+            Event.objects.create(title='Test Event', start_date='4444x02a00').full_clean()
+        with self.assertRaises(ValidationError):
+            Event.objects.create(title='Test Event', start_date='4444-02-00').full_clean()
+        with self.assertRaises(ValidationError):
+            Event.objects.create(title='Test Event', start_date='2015-00-22').full_clean()
+        with self.assertRaises(ValidationError):
+            Event.objects.create(title='Test Event', start_date='2015-02-30').full_clean()
+        with self.assertRaises(ValidationError):
+            Event.objects.create(title='Test Event', start_date='2015-02-30', end_date='2014-01-02').full_clean()
