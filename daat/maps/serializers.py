01d9d4499e697d8e18e2e6c5be2097b6e27dc932
Yuval Adam <yuval@y3xz.com>
2015-12-24 17:40:18 +0200

    Add rudimentary Place nested relation in Event API"

--- daat/maps/serializers.py
+++ daat/maps/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from .models import *
 
 
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+
+
 class EventSerializer(serializers.ModelSerializer):
+    place = PlaceSerializer(read_only=True)
     class Meta:
         model = Event
