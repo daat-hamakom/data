aebd4d4c723af0fb4ceeed496842be9fce03003a
Yuval Adam <yuval@y3xz.com>
2016-01-18 00:22:54 +0200

    Return basic fields on /events API

--- daat/maps/serializers.py
+++ daat/maps/serializers.py
@@ -6,9 +6,11 @@ from .models import *
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
+        fields = ('id', 'name', 'position')
 
 
 class EventSerializer(serializers.ModelSerializer):
     place = PlaceSerializer(read_only=True)
     class Meta:
         model = Event
+        fields = ('id', 'title', 'subtitle', 'start_date', 'end_date', 'place')
