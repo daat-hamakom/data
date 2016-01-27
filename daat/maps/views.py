7efb920fa7372666b6e5acdd8013fada89adc8a1
Yuval Adam <yuval@y3xz.com>
2016-01-27 23:29:52 +0200

    Add raw /sprites CBV

--- daat/maps/views.py
+++ daat/maps/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.views.generic import View
+
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework import viewsets
@@ -9,3 +12,9 @@ from .serializers import *
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class SpriteView(View):
+    def get(self, *args, **kwargs):
+        response = HttpResponse('hai')
+        return response
