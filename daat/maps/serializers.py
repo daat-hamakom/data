b6f0f35db03bd46d2494a96cd76e3306b850a384
Yuval Adam <yuval@y3xz.com>
2016-02-29 17:28:25 +0200

    Add people, orgs and media to events API

--- daat/maps/serializers.py
+++ daat/maps/serializers.py
@@ -9,11 +9,33 @@ class PlaceSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'position')
 
 
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ('id', 'first_name', 'middle_name', 'last_name')
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ('id', 'name')
+
+
+class MediaSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Media
+		fields = ('file',)
+
+
 class EventSerializer(serializers.ModelSerializer):
 
     place = PlaceSerializer(read_only=True)
+    people = PersonSerializer(read_only=True, many=True)
+    organizations = OrganizationSerializer(read_only=True, many=True)
+    media = MediaSerializer(read_only=True, many=True)
     project = serializers.SlugRelatedField(read_only=True, slug_field='title')
 
     class Meta:
         model = Event
-        fields = ('id', 'title', 'subtitle', 'start_date', 'end_date', 'place', 'description', 'icon', 'project')
+        fields = ('id', 'title', 'subtitle', 'start_date', 'end_date', 'place',
+        	'description', 'icon', 'project', 'people', 'organizations', 'media')
