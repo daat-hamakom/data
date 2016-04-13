442ce3d9587a1f0e8d004acdeaff10b7a9cf4b54
Yuval Adam <yuval@y3xz.com>
2016-04-13 15:12:47 +0300

    Add refresh thumbs mng task

---
+++ daat/maps/management/commands/refresh_thumbs.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.core.cache import cache
+from django.core.management.base import BaseCommand, CommandError
+
+from daat.maps.utils import SpriteCreator
+from daat.maps.models import *
+from daat.maps.tasks import gen_image_thumbnails
+
+class Command(BaseCommand):
+    help = 'Refresh image media thumbnails'
+
+    def handle(self, *args, **options):
+        for im in Media.objects.filter(type='image'):
+            print(im)
+            try:
+                gen_image_thumbnails(im)
+            except e:
+                print(e)
