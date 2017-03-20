8d70f318162ec2692c924b357105d9202277ab10
talbenbasat <talbenbasat@gmail.com>
2017-03-20 19:14:57 +0200

    #general - hotfix

--- daat/urls.py
+++ daat/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     url(r'^s3direct/', include('s3direct.urls')),
     url(r'^select2/', include('django_select2.urls')),
 ]
-#
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
