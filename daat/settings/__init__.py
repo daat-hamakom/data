b5b4b0d36aae99022c4e75cccaecc7d094873e65
Tal <tal@talbenbasat.com>
2017-06-14 19:16:54 +0300

    Added staging conf

--- daat/settings/__init__.py
+++ daat/settings/__init__.py
@@ -2,5 +2,7 @@ from os import environ
 
 if 'PROD' in environ:
 	from .prod import *
+elif 'STAG' in environ:
+	from .staging import *
 else:
 	from .local import *
---
+++ daat/settings/staging.py
@@ -0,0 +1,70 @@
+import dj_database_url
+from os import environ
+from urllib.parse import urlparse
+
+from .base import *
+
+ENV = 'prod'
+
+ALLOWED_HOSTS = [
+    'daat-hamakom-data-staging.herokuapp.com'
+]
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
+
+BROKER_URL = REDIS_URL = environ.get('REDIS_URL')
+redis_url = urlparse(REDIS_URL)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '{}:{}'.format(redis_url.hostname, redis_url.port),
+        'OPTIONS': {
+            'DB': 0,
+            'PASSWORD': redis_url.password,
+            'PARSER_CLASS': 'redis.connection.HiredisParser',
+            'CONNECTION_POOL_CLASS': 'redis.BlockingConnectionPool',
+            'CONNECTION_POOL_CLASS_KWARGS': {
+                'max_connections': 5,
+                'timeout': 20
+            }
+        }
+    }
+}
+
+INSTALLED_APPS += (
+    's3direct',
+)
+
+CELERY_ALWAYS_EAGER = True
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = str(PROJECT_DIR / 'staticroot')
+STATIC_URL = '/static/'
+
+RAVEN_CONFIG = {
+    'dsn': environ.get('RAVEN_DSN')
+}
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = environ.get('AWS_BUCKET_NAME')
+
+S3DIRECT_REGION = 'eu-west-1'
+S3DIRECT_DESTINATIONS = {
+    'media': ('media', lambda u: u.is_authenticated()),
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 86400  # this also enables the HSTS header
