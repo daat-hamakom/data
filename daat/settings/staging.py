d2422084458aeb3aba90e2ee7b488b34ac78b200
Tal <tal@talbenbasat.com>
2017-07-05 10:30:49 +0300

    Reverted debug mode

--- daat/settings/staging.py
+++ daat/settings/staging.py
@@ -6,7 +6,6 @@ from ..utils import create_filename
 from .base import *
 
 ENV = 'stag'
-DEBUG = True
 
 ALLOWED_HOSTS = [
     'daat-hamakom-data-staging.herokuapp.com'
@@ -47,8 +46,6 @@ CELERY_ALWAYS_EAGER = False
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = str(PROJECT_DIR / 'staticroot')
 STATIC_URL = '/static/'
-MEDIA_ROOT = str(PROJECT_DIR / 'media')
-MEDIA_URL = '/media/'
 
 RAVEN_CONFIG = {
     'dsn': environ.get('RAVEN_DSN')
